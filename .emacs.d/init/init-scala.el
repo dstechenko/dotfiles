49d956b879392a6ccbd20e7f5097c6b6a3241b49
Dmytro Stechenko <dmytro.stechenko@gmail.com>
2017-03-18 19:03:46 -0400

    Drop ensime configuration

--- .emacs.d/init/init-scala.el
+++ .emacs.d/init/init-scala.el
@@ -9,7 +9,6 @@
 ;;
 ;; Includes the following:
 ;; - scala-mode
-;; - ensime
 ;;
 
 ;;; Code:
@@ -17,8 +16,8 @@
 (require 'init-common)
 
 (use-package scala-mode
-  :commands
-  scala-indent:insert-asterisk-on-multiline-comment
+  :pin
+  melpa-stable
 
   :interpreter
   ("scala" . scala-mode)
@@ -26,73 +25,59 @@
   :bind
   ("H-s" . scala-mode))
 
-(use-package ensime
-  :pin
-  melpa-stable
-
-  :commands
-  (ensime
-   ensime-mode
-   ensime-reload)
-
-  :config
-  (require 'ensime-expand-region)
-
-  :bind
-  ("H-e" . ensime)
-  ("H-E" . ensime-reload))
-
-;; Set newline functionality for comments
-(defun scala-mode-newline-comments ()
-  "Custom newline appropriate for `scala-mode'."
-  (interactive)
-  (newline-and-indent)
-  (scala-indent:insert-asterisk-on-multiline-comment))
-
-;; Set parenthesis
-(sp-local-pair 'scala-mode "(" nil :post-handlers '(("||\n[i]" "RET")))
-(sp-local-pair 'scala-mode "{" nil :post-handlers '(("||\n[i]" "RET") ("| " "SPC")))
-
-;; Set parenthesis navigation
-(defun sp-restrict-c (sym)
-  "Smartparens restriction on `SYM' for C-derived parenthesis."
+;; Set parenthesis restriction
+(defun scala-restrict-sp (sym)
+  "Parenthesis restriction on `SYM' for Scala."
   (sp-restrict-to-pairs-interactive "{([" sym))
 
 ;; Set formatting function
 (defun run-cli-scalafmt ()
-  "Format current project using `scalafmt' command line interface."
+  "Format current file using `scalafmt' command line interface."
   (interactive)
-  (if (projectile-project-p)
-      (call-process "scalafmt")
-    (message "Not in a project to format code")))
+  (call-process "scalafmt" nil nil nil (format "-i -f %s" (buffer-file-name))))
 
-;; Set bindings
+;; Set key bindings
 (defun scala-mode-bindings ()
   "Bind all Scala custom keys."
   (bind-key "H-l" 'run-cli-scalafmt scala-mode-map)
-  (bind-key "RET" 'scala-mode-newline-comments scala-mode-map)
-  (bind-key "s-<delete>" (sp-restrict-c 'sp-kill-sexp) scala-mode-map)
-  (bind-key "s-<backspace>" (sp-restrict-c 'sp-backward-kill-sexp) scala-mode-map)
-  (bind-key "s-<home>" (sp-restrict-c 'sp-beginning-of-sexp) scala-mode-map)
-  (bind-key "s-<end>" (sp-restrict-c 'sp-end-of-sexp) scala-mode-map)
-  (bind-key "s-{" 'sp-rewrap-sexp smartparens-mode-map)
-  (bind-key "C-<tab>" 'dabbrev-expand scala-mode-map))
+  (bind-key "M-<return>" 'dabbrev-expand scala-mode-map)
+  (bind-key "s-<delete>" (scala-restrict-sp 'sp-kill-sexp) scala-mode-map)
+  (bind-key "s-<backspace>" (scala-restrict-sp 'sp-backward-kill-sexp) scala-mode-map)
+  (bind-key "s-<home>" (scala-restrict-sp 'sp-beginning-of-sexp) scala-mode-map)
+  (bind-key "s-<end>" (scala-restrict-sp 'sp-end-of-sexp) scala-mode-map)
+  (bind-key "s-{" 'sp-rewrap-sexp smartparens-mode-map))
 
-;; Set hooks
+;; Set multi-line comments
+(defun scala-mode-multi-line-comments ()
+  "Bind multi-line comments."
+  (setq comment-start "/* "
+        comment-end   " */"
+        comment-style 'multi-line
+        comment-empty-lines t))
+
+(defun scala-mode-prettify-symbols ()
+  "Bind prettified symbols."
+  (setq prettify-symbols-alist scala-prettify-symbols-alist)
+  (prettify-symbols-mode))
+
+(defun scala-mode-smartparens ()
+  "Bind parenthesis padding."
+  (sp-local-pair 'scala-mode "(" nil :post-handlers '(("||\n[i]" "RET")))
+  (sp-local-pair 'scala-mode "{" nil :post-handlers '(("||\n[i]" "RET") ("| " "SPC"))))
+
+;; Set mode hook
 (add-hook 'scala-mode-hook
           (lambda ()
-            (setq comment-start "/* "
-                  comment-end " */"
-                  comment-style 'multi-line
-                  comment-empty-lines t)
-
-            (scala-mode-bindings)
             (show-paren-mode)
             (smartparens-mode)
             (yas-minor-mode)
             (git-gutter-mode)
             (company-mode)
-            (ensime-mode)
+
+            (scala-mode-bindings)
+            (scala-mode-multi-line-comments)
+            (scala-mode-prettify-symbols)
+            (scala-mode-smartparens)
 
             (scala-mode:goto-start-of-code)))
 
