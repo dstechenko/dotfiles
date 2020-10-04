60d6d7807f91a37b42ca097b1f4613fdd4c0012a
Dmytro Stechenko <dmytro.stechenko@gmail.com>
2020-10-04 18:38:24 -0400

    add default config file

---
+++ .emacs.d/init.el
@@ -0,0 +1,14 @@
+;;; init.el --- Default Emacs configuration
+
+;; Copyright (C) Dmytro Stechenko
+;; License: http://www.gnu.org/licenses/gpl.html
+
+;;; Code:
+
+(add-to-list
+ 'load-path
+ (expand-file-name "init" user-emacs-directory))
+
+(require 'init-shared)
+
+;;; init.el ends here
