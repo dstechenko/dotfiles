ab1ec29d38fea59c4d52b2a7772c72ae010825c0
Dmytro Stechenko <dmytro.stechenko@gmail.com>
2020-10-04 18:49:36 -0400

    fix shared emacs config install

--- .emacs.d/install_shared.sh
+++ .emacs.d/install_shared.sh
@@ -8,7 +8,7 @@ set -euxo pipefail
          EMACS_HOME="$HOME/.emacs.d"
 DOTFILES_EMACS_SRCS="$DOTFILES_INSTALL_SRCS/emacs"
 
-rm    -rf "$EMACS_HOME"
+rm    -rf "$EMACS_HOME/init/init-shared.el"
 mkdir -p  "$EMACS_HOME/init"
 cp        "$DOTFILES_EMACS_SRCS/init-shared.el" \
           "$EMACS_HOME/init/init-shared.el"
