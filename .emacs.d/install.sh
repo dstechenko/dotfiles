385bffd3eb3e45e98735133171791d2f98869838
Dmytro Stechenko <dmytro.stechenko@gmail.com>
2020-10-04 18:46:06 -0400

    add default emacs config install script

---
+++ .emacs.d/install.sh
@@ -0,0 +1,16 @@
+  
+#!/bin/bash
+
+# Copyright (C) Dmytro Stechenko
+# License: http://www.gnu.org/licenses/gpl.html
+
+set -euxo pipefail
+
+         EMACS_HOME="$HOME/.emacs.d"
+DOTFILES_EMACS_SRCS="$DOTFILES_INSTALL_SRCS/emacs"
+
+rm -rf "$EMACS_HOME"
+
+# shellcheck source=/dev/null
+source "$DOTFILES_EMACS_SRCS/install_shared.sh"
+cp "$DOTFILES_EMACS_SRCS/init.el" "$EMACS_HOME/init.el"
