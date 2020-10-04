ca1e1026b0f762cda67dedcf1aee85321ea0216e
Dmytro Stechenko <dmytro.stechenko@gmail.com>
2020-10-04 19:28:45 -0400

    add dotfiles install script

---
+++ install.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Copyright (C) Dmytro Stechenko
+# License: http://www.gnu.org/licenses/gpl.html
+
+set -euxo pipefail
+
+CALL_DIR=$(pwd)
+DOTFILES_INSTALL_SRCS=$(dirname "$0")
+
+cd "$DOTFILES_INSTALL_SRCS"
+DOTFILES_INSTALL_SRCS=$(pwd -P)
+
+DOTFILES_EMACS_SRCS="$DOTFILES_INSTALL_SRCS/emacs"
+ DOTFILES_BASH_SRCS="$DOTFILES_INSTALL_SRCS/bash"
+  DOTFILES_GIT_SRCS="$DOTFILES_INSTALL_SRCS/git"
+
+# shellcheck source=/dev/null
+source "DOTFILES_EMACS_SRCS/install.sh"
+
+# shellcheck source=/dev/null
+source "DOTFILES_BASH_SRCS/install.sh"
+
+# shellcheck source=/dev/null
+source "DOTFILES_GIT_SRCS/install.sh"
+
+cd "$CALL_DIR"
