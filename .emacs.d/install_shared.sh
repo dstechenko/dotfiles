f4ea958e5cfec26907cdf3cd720df4264bc812b3
Dmytro Stechenko <dmytro.stechenko@gmail.com>
2020-10-04 18:32:29 -0400

    add script to install shared config

---
+++ .emacs.d/install_shared.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Copyright (C) Dmytro Stechenko
+# License: http://www.gnu.org/licenses/gpl.html
+
+set -euxo pipefail
+
+         EMACS_HOME="$HOME/.emacs.d"
+DOTFILES_EMACS_SRCS="$DOTFILES_INSTALL_SRCS/emacs"
+
+rm    -rf "$EMACS_HOME"
+mkdir -p  "$EMACS_HOME/init"
+cp        "$DOTFILES_EMACS_SRCS/init-shared.el" \
+          "$EMACS_HOME/init/init-shared.el"
