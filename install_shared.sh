1c6159dd8e235875a6d1d6aa60f0e2c365b4ca89
Dmytro Stechenko <dmytro.stechenko@gmail.com>
2020-10-04 19:41:01 -0400

    add install shared top level script

---
+++ install_shared.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Copyright (C) Dmytro Stechenko
+# License: http://www.gnu.org/licenses/gpl.html
+
+set -euxo pipefail
+
+CALL_DIR=$(pwd)
+DOTFILES_INSTALL_SRCS=$(dirname "$0")
+
+cd "$DOTFILES_INSTALL_SRCS"
+DOTFILES_INSTALL_SRCS=$(pwd -P)
+
+DOTFILES_EMACS_SRCS="$DOTFILES_INSTALL_SRCS/emacs"
+
+# shellcheck source=/dev/null
+source "DOTFILES_EMACS_SRCS/install_shared.sh"
+
+cd "$CALL_DIR"
