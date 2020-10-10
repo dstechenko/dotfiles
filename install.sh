67634d676196b7092fc5cf9d2c2d1a18cfacf5ad
Dmytro Stechenko <dmytro.stechenko@gmail.com>
2020-10-10 13:05:42 -0400

    fix install repo path fidner

--- install.sh
+++ install.sh
@@ -4,16 +4,15 @@
 
 set -euxo pipefail
 
-CALL_DIR=$(pwd)
-DOTFILES_INSTALL_SRCS=$(dirname "$0")
-
-cd "$DOTFILES_INSTALL_SRCS"
-DOTFILES_INSTALL_SRCS=$(pwd -P)
+             CALL_DIR=$(pwd)
+DOTFILES_INSTALL_SRCS=$(dirname  "$0")
+DOTFILES_INSTALL_SRCS=$(realpath "$DOTFILES_INSTALL_SRCS")
 
 DOTFILES_EMACS_SRCS="$DOTFILES_INSTALL_SRCS/emacs"
  DOTFILES_BASH_SRCS="$DOTFILES_INSTALL_SRCS/bash"
   DOTFILES_GIT_SRCS="$DOTFILES_INSTALL_SRCS/git"
-  
+
+cd "$DOTFILES_INSTALL_SRCS"
 git pull origin master
 
 # shellcheck source=/dev/null
