46c5ad009de10c1ffc616d8d6ff199b83c4cab2d
Dmytro Stechenko <dmytro.stechenko@gmail.com>
2020-10-10 13:06:59 -0400

    fix install shared repo finder

--- install_shared.sh
+++ install_shared.sh
@@ -4,14 +4,13 @@
 
 set -euxo pipefail
 
-CALL_DIR=$(pwd)
-DOTFILES_INSTALL_SRCS=$(dirname "$0")
-
-cd "$DOTFILES_INSTALL_SRCS"
-DOTFILES_INSTALL_SRCS=$(pwd -P)
+             CALL_DIR=$(pwd)
+DOTFILES_INSTALL_SRCS=$(dirname  "$0")
+DOTFILES_INSTALL_SRCS=$(realpath "$DOTFILES_INSTALL_SRCS")
 
 DOTFILES_EMACS_SRCS="$DOTFILES_INSTALL_SRCS/emacs"
 
+cd "$DOTFILES_INSTALL_SRCS"
 git pull origin master
 
 # shellcheck source=/dev/null
