bde09d22c0152691d30f855c8e141369a68fbbd2
Dmytro Stechenko <dmytro.stechenko@gmail.com>
2020-10-10 13:09:31 -0400

    fix comments

--- emacs/init.el
+++ emacs/init.el
@@ -1,7 +1,6 @@
 ;;; init.el --- Default Emacs configuration
-
-;; Copyright (C) Dmytro Stechenko
-;; License: http://www.gnu.org/licenses/gpl.html
+;;; Copyright (C) Dmytro Stechenko
+;;; License: http://www.gnu.org/licenses/gpl.html
 
 ;;; Code:
 
