3cde3cae56b6b9ff7e8dbc5516c713b8935bb75a
Dmytro Stechenko <dmytro.stechenko@gmail.com>
2023-07-13 12:36:49 -0400

    [media] fix install paths (again)

--- media/install.sh
+++ media/install.sh
@@ -4,10 +4,10 @@
 
 set -euxo pipefail
 
-rm -rf "${HOME:?}/Pictures/wallpaper.*"
-cp     "$INSTALL_SRCS/media/wallpaper.*" \
+rm -rf "${HOME:?}/Pictures/wallpaper.png"
+cp     "$INSTALL_SRCS/media/wallpaper.png" \
        "${HOME:?}/Pictures/"
 
-rm -rf "${HOME:?}/Pictures/profile.*"
-cp     "$INSTALL_SRCS/media/profile.*" \
+rm -rf "${HOME:?}/Pictures/profile.png"
+cp     "$INSTALL_SRCS/media/profile.png" \
        "${HOME:?}/Pictures/"
