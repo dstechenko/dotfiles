d458b6417f5126ee77b77bf94ef57b08672fb701
Dmytro Stechenko <dmytro.stechenko@gmail.com>
2024-09-20 08:38:27 -0400

    [vscode] remove emacs and add settings

---
+++ vscode/copy-local.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Copyright (C) Dmytro Stechenko
+# License: http://www.gnu.org/licenses/gpl.html
+
+set -euxo pipefail
+
+  CALL_DIR=$(pwd)
+VSCODE_DIR=$(realpath $(dirname  "$0"))
+
+cp ~/Library/Application\ Support/Code/User/settings.json    "${VSCODE_DIR}"
+cp ~/Library/Application\ Support/Code/User/keybindings.json "${VSCODE_DIR}"
+
+cd "${CALL_DIR}"
