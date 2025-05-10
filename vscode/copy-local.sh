f7b9f364ea3ae9a99369fd1849838f42b61ffdf8
Dmytro Stechenko <dmytro.stechenko@gmail.com>
2025-05-10 11:02:43 -0400

    vscode: fix install scripts again and again

--- vscode/copy-local.sh
+++ vscode/copy-local.sh
@@ -4,10 +4,7 @@
 
 set -euxo pipefail
 
-  CALL_DIR=$(pwd)
 VSCODE_DIR=$(realpath $(dirname  "$0"))
 
 cp ${HOME:?}/Library/Application\ Support/*Code*/User/settings.json    "${VSCODE_DIR}"
 cp ${HOME:?}/Library/Application\ Support/*Code*/User/keybindings.json "${VSCODE_DIR}"
-
-cd "${CALL_DIR}"
--- vscode/install.sh
+++ vscode/install.sh
@@ -4,10 +4,5 @@
 
 set -euxo pipefail
 
-  CALL_DIR=$(pwd)
-VSCODE_DIR=$(realpath $(dirname  "$0"))
-
 cp "$INSTALL_SRCS/vscode/settings.json"    ${HOME:?}/Library/Application\ Support/*Code*/User/
 cp "$INSTALL_SRCS/vscode/keybindings.json" ${HOME:?}/Library/Application\ Support/*Code*/User/
-
-cd "${CALL_DIR}"
