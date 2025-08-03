dfb8e84d2dd491715b48e72a3e58aaf234ee3a7a
Dmytro Stechenko <dmytro.stechenko@gmail.com>
2025-08-03 09:19:01 -0400

    [media] remove wallpaper

--- media/install.sh
+++ media/install.sh
@@ -4,10 +4,6 @@
 
 set -euxo pipefail
 
-rm -rf "${HOME:?}/Pictures/wallpaper.png"
-cp     "$INSTALL_SRCS/media/wallpaper.png" \
-       "${HOME:?}/Pictures/"
-
 rm -rf "${HOME:?}/Pictures/profile.jpg"
 cp     "$INSTALL_SRCS/media/profile.jpg" \
        "${HOME:?}/Pictures/"
