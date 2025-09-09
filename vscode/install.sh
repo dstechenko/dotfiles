277af68af2987d6f3d5e9f91a32acbc3486e3877
Dmytro Stechenko <dmytro.stechenko@gmail.com>
2025-09-09 10:09:55 -0400

    vscode: remove configs

--- vscode/install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Copyright (C) Dmytro Stechenko
-# License: http://www.gnu.org/licenses/gpl.html
-
-set -euxo pipefail
-
-cp "$INSTALL_SRCS/vscode/settings.json"    ${HOME:?}/Library/Application\ Support/*Code*/User/
-cp "$INSTALL_SRCS/vscode/keybindings.json" ${HOME:?}/Library/Application\ Support/*Code*/User/
